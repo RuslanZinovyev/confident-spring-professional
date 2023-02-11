a05539b75d427b3d876abd57798dd9a74f1a2db0
rzinovyev <ruslan.zinovyev@hotmail.com>
2023-02-11 09:05:11 -0500

    add create implementation

--- src/main/java/com/myfancypfdinvoices/service/InvoiceService.java
+++ src/main/java/com/myfancypfdinvoices/service/InvoiceService.java
@@ -5,18 +5,20 @@ import com.myfancypfdinvoices.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class InvoiceService {
-
-    private final UserService userService;
-
     private final String cdnUrl;
     private final JdbcTemplate jdbcTemplate;
     private final List<Invoice> invoices = new CopyOnWriteArrayList<>();
@@ -24,9 +26,8 @@ public class InvoiceService {
     @Autowired
     public InvoiceService(UserService userService,
                           @Value("${cdn.url}")
-                          String cdnUrl,
+                                  String cdnUrl,
                           JdbcTemplate jdbcTemplate) {
-        this.userService = userService;
         this.cdnUrl = cdnUrl;
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -48,13 +49,27 @@ public class InvoiceService {
     }
 
     public Invoice create(String userId, Integer amount) {
-        User user = userService.findById(userId);
-        if (user == null) {
-            throw new IllegalStateException();
-        }
+        String generatedPfgUrl = cdnUrl + "/images/default/sample.pdf";
 
-        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
-        invoices.add(invoice);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement("INSERT INTO invoices (user_id, pdf_url, amount) VALUES (?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, userId);
+            ps.setString(2, generatedPfgUrl);
+            ps.setInt(3, amount);
+            return ps;
+        }, keyHolder);
+
+        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString() : null;
+
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPfgUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
 
         return invoice;
     }
